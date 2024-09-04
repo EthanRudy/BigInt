a53c1b240292f4a274e3836cc88ca3a9e8d8689a
Ethan Rudy <ethan.c.rudy@gmail.com>
2024-09-04 11:21:49 -0700

    Fixed random big::Integer generator

--- BigInt.hpp
+++ BigInt.hpp
@@ -6,6 +6,7 @@
 #include <exception>
 #include <iomanip>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -402,21 +403,21 @@ namespace big {
 
 		/**
 		* Get Length (Chunk)
-		* 
+		*
 		* @returns the length of the num vector
 		*/
 		unsigned int getLength();
 
 		/**
 		* Get Length (Digits)
-		* 
+		*
 		* @returns the number of digits in the num vector
 		*/
 		unsigned int getDigitLength();
 
 		/**
 		* Get Chunk
-		* 
+		*
 		* @returns the chunk at a specified index
 		*/
 		unsigned int getChunk(unsigned int n);
@@ -1518,52 +1519,41 @@ namespace big {
 
 	// Random (range)
 	Integer random(big::Integer min, big::Integer max) {
+		if (max < min) {
+			throw IntegerException("Maximum random value cannot exceed the specified minimum");
+		}
+
 		std::random_device rd;
 		std::mt19937 gen(rd());
-		std::uniform_int_distribution<> dist(1, 1000000000);
+		Integer random_big;
 
-		int minSize = min.getLength(), maxSize = max.getLength();
-		int size;
-		if (minSize == maxSize) { size = minSize; }
-		else {
-			size = rand() % (maxSize - minSize) + minSize;
+		// Negative number check?
+		bool negative = false;
+		Integer offset = -min;
+		if (min < 0) { 
+			negative = true;
+			min += offset, max += offset; 
 		}
 
-		int minChunk = min.getChunk(minSize - 1), maxChunk = max.getChunk(maxSize - 1);
-		int diff = maxChunk - minChunk;
-		if (diff == 0) { diff = 1; }
+		int min_size = min.getLength(), max_size = max.getLength();
 
-		Integer num;
-		big::Integer newChunk;
-		for (int i = 0; i < size - 1; ++i) {
-			newChunk = dist(gen);
-			if (i != 0) {
-				newChunk *= BASE * i;
+		for (int i = 0; i < max_size; ++i) {
+			int min_rand = 0, max_rand = BASE;
+			if (i == (min_size - 1)) {
+				min_rand = min.getChunk(i);
+			}
+			if (i == (max_size - 1)) {
+				max_rand = max.getChunk(i);
 			}
 
-			num += newChunk;
+			std::uniform_int_distribution<> dist(min_rand, max_rand - 1);
+			Integer rand_block = dist(gen);
+			random_big += rand_block * ((i > 0) ? (BASE * (i)) : 1);
 		}
 
-		if (minSize == maxSize) {
-			if (minChunk == maxChunk) { newChunk = minChunk; }
-			else {
-				newChunk = dist(gen) % (diff)+minChunk;
-			}
-		}
-		else if (size < maxSize) {
-			newChunk = dist(gen) % BASE;
-		}
+		if (negative) { random_big -= offset; }
+		return random_big;
 
-		if (size == 1) {
-			num = newChunk;
-		}
-		else {
-			num += (newChunk * (BASE * size));
-		}
-
-		//std::cout << "\n" << min << " " << num << " " << max << "\n";
-
-		return num;
 	}
 }
 
